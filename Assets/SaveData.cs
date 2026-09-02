0d31cae2f0de77dba0b4d2c162f016c61331a9c7
aLISEkATE <IPA23.K.STRAZDINA@VTDT.EDU.LV>
2026-09-02 16:48:56 +0300

    unity setup + everything i have made so far before  school started

---
+++ Assets/SaveData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+[System.Serializable]
+public class SaveData
+{
+public Vector3 playerPosition;
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuController : MonoBehaviour
+{
+
+    public GameObject menuCanvas;
+
+    void Start()
+    {
+        menuCanvas.SetActive(false);
+        
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.tabKey.wasPressedThisFrame)
+        {
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+        }
+    }
+}
