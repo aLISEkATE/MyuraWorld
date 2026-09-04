581c6df466274e8d1c0e48aeff784c9369e13019
aLISEkATE <IPA23.K.STRAZDINA@VTDT.EDU.LV>
2026-09-04 13:51:50 +0300

    Item Pickup done and functional

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -14,6 +14,23 @@ public class InventoryController : MonoBehaviour
         itemDictionary = FindFirstObjectByType<ItemDictionary>(); 
     }
 
+    public bool AddItem(GameObject itemPrefab)
+    {
+        foreach(Transform slotTransform in inventoryPanel.transform)
+        {
+            Slot slot = slotTransform.GetComponent<Slot>();
+            if (slot != null &&  slot.currentItem == null)
+            {
+                GameObject newItem = Instantiate(itemPrefab, slotTransform);
+                newItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = newItem;
+                return true;
+            }
+        }
+
+        Debug.Log("Inventory is full");
+        return false;
+    }
     public List<InventorySaveData> GetInventoryItems()
     {
         List<InventorySaveData> invData = new List<InventorySaveData>();
